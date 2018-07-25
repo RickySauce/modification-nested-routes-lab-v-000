7974ee78664eaf760ce01ba6748f6376f147ff8e
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-25 14:30:02 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,10 +5,6 @@ module ArtistsHelper
 
   def artist_select(artist, song)
     binding.pry
-    if song.artist.nil?
-     select_tag "song_artist_id", options_from_collection_for_select(Artist.all, :id, :name)
-    else
-      hidden_field_tag "song_artist_id", song.artist_id
-     end
    end
+
 end
